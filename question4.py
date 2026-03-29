fbcaf349a1bfce8dfae84b4d86e58aa61a144d0b
devpaliwal1421-cyber <devpaliwal1421@gmail.com>
2026-03-30 00:02:56 +0530

    Add Fibonacci series program in question4.py
    Implement Fibonacci series generator up to n terms.

---
+++ question4.py
@@ -0,0 +1,11 @@
+Write a Python program to print Fibonacci series up to n terms
+
+#CODE
+
+n = int(input("Enter number of terms: "))
+
+a, b = 0, 1
+
+for i in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
